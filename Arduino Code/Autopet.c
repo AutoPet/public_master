1db64689134091bb70575a022847e3a84634379b
gilbertorobles2 <44030148+gilbertorobles24@users.noreply.github.com>
2021-11-13 18:44:09 +0000

    Updated arduino pseudo code

---
+++ Arduino Code/Autopet.c
@@ -0,0 +1,15 @@
+#include <Servo.h>
+
+Servo servo;
+
+void setup(){
+  servo.attach(6);
+}
+
+void loop(){
+  //get input data stream from wifi module
+
+  //get servo value from stream
+
+  //assign servo value
+}
